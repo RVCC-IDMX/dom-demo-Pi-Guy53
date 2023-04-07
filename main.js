9735e391bcc698db3108bf55f87b10cea1f48b94
UsrnGamesInc <71029215+Pi-Guy53@users.noreply.github.com>
2023-04-07 16:09:39 -0400

    working form from demo

--- main.js
+++ main.js
@@ -1,8 +1,56 @@
-/* ATTENTION: THIS IS CODE FROM THE YOUTUBE CRASH COURSE. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES 
+/* ATTENTION: THIS IS CODE FROM THE YOUTUBE CRASH COURSE. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES
 
 Video by Brad Traversy
 JavaScript Crash Course For Beginners
-https://youtu.be/hdI2bqOjy3c?t=4228 
+https://youtu.be/hdI2bqOjy3c?t=4228
 
 Start at 1:10:29
  */
+
+// const ul = document.querySelector('.items');
+// // ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = 'Hello';
+// ul.children[1].innerText = 'Brad';
+// ul.lastElementChild.innerHTML = '<h1> Hello </h1>'
+
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
+
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
+
+// btn.addEventListener('mouseout', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+// });
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();;
+
+    if (nameInput.value === '' || emailInput.value === '') {
+        msg.innerHTML = 'Please enter all fields';
+        msg.classList.add('error');
+
+        //setTimeout(() => msg.remove(), 3000); //removes msg, not hide it, does not show/work second time around
+        setTimeout(() => { msg.innerHTML = ''; msg.classList.remove('error') }, 3000); //Resetsthe msg value to allow reuse
+    }
+    else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userList.appendChild(li);
+
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
